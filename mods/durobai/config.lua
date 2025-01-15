2c93125699387c5e058dca974e9ca48f60eac5c8
MadCake <placeholder@email.com>
2025-01-16 00:53:24 +0100

    fix d20 promotion

--- mods/durobai/config.lua
+++ mods/durobai/config.lua
@@ -6,7 +6,7 @@ local config = {
         dice = Array:new({"durobai_die_blue", "durobai_die_yellow"}),
         promotionsBag = "durobai_reference_dice"
     },
-    promotions = {[6] = 8, [8] = 10, [10] = 12, [12] = 20},
+    promotions = {[6] = 8, [8] = 10, [10] = 12, [12] = 20, [20] = 20},
     defenseOverride = {[20] = 6},
     attackBonus = 3
 }
